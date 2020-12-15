cc83edfa3438d23f43f81433fc2a24c00b970bf7
AviNazari <44567819+AviNazari@users.noreply.github.com>
2020-12-15 22:45:16 +0100

    update_basket_line added

--- api_task.py
+++ api_task.py
@@ -1,3 +1,9 @@
+# How-to for: "2. Upload into a basket"
+
+# The requests library must be installed.
+# Please update base_url, token and
+# filename according to your configuration.
+
 import json
 import time
 
@@ -5,7 +11,7 @@ import requests
 
 base_url = "https://testtask.3yourmind.com/api/v2.0/"
 token = "792b74f003a2e05ae81eaae96b11a873b229ac40"
-filename = "Left_arm.stl"  
+filename = "Left_arm.stl"  # placed in the same directory
 
 std_header = {"Authorization": f"Token {token}"}
 
@@ -56,8 +62,27 @@ def wait_for_optimization(basket_id, line_id):
     return
 
 
+def update_basket_line(basket_id, line_id):
+    print("update_basket_line")
+    params = {"basket_id": basket_id, "line_id": line_id, "postProcessings": [
+        {
+          "colorId": 0,
+          "postProcessingId": 0
+        }
+      ]}
+
+    r = requests.patch(
+        url=f"{base_url}user-panel/baskets/{basket_id}/lines/{line_id}/",
+        headers=std_header,
+        params=params,
+    )
+    print(str(r.__dict__))
+    return r
+
+
 print("create_order")
 basket_id = create_basket()
 line_id = create_line(basket_id)
 upload_file(basket_id, line_id)
+update_basket_line(basket_id, line_id)
 
